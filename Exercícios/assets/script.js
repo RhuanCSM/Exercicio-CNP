30a0b0c17ff90c0f47ddf1812485aac2fde38d52
RhuanCSM <rcsm.martin@gmail.com>
2024-04-18 16:01:46 -0300

    notes_add

--- Exercícios/assets/script.js
+++ Exercícios/assets/script.js
@@ -1,24 +1,3 @@
+const startGame = document.getElementById('btn')
 
 
-
-const alo = document.querySelector('#oi')
-
-alo.addEventListener('click', () => {
-    console.log('deu certo')
-})
-
-
-const ab = 'teste'
-
-
-
-if (ab === 'teste') {
-     a = 'a'
-}
-
-
-const funcao = () => {
-    const a = 'teste'
-    // console.log(a)
-}
-funcao()
