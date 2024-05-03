bebe0a197d92d5fbe39d98059686143487736bd5
RhuanCSM <rcsm.martin@gmail.com>
2024-05-03 09:31:40 -0300

    notes_add

---
+++ Exercícios/assets/script.js
@@ -0,0 +1,22 @@
+
+const productsList = {
+  products: [
+    {
+      title: 'A pillow', 
+      imageUrl: 
+        'https://www.coyuchi.com/cdn/shop/files/ACC-Dec_LostCoastOrganic_PillowCover_Redwood_A_FA23_16377_1.jpg?v=1695925578', 
+      price: 19.99, 
+      description: 'A soft pillow!'
+    },
+    {
+      title: 'A Carpet', 
+      imageUrl: 
+        'https://m.media-amazon.com/images/I/91zYlOJHPFL._AC_UF894,1000_QL80_.jpg', 
+      price: 89.99, 
+      description: 'A carpet which  you might like – or not.'
+    }
+  ],
+  render() {
+    const renderHook =
+  }
+}
